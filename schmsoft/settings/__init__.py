d082fd9db372b851f3891a6cd297d26d7f41a563
Henry Ohanga <ohanga.henry@gmail.com>
2021-12-02 18:08:56 +0300

    Correct handling of envs

--- schmsoft/settings/__init__.py
+++ schmsoft/settings/__init__.py
@@ -13,9 +13,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 from datetime import timedelta
-from dotenv import load_dotenv
-
-load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
@@ -101,7 +98,6 @@ DATABASES = {
         "USER": os.environ.get("POSTGRES_USER"),
         "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
         "HOST": os.environ.get("POSTGRES_HOST"),
-        "PORT": os.environ.get("POSTGRES_PORT"),
     }
 }
 
