7d527f88f03142688ba3369310c41a514563a527
Henry Ohanga <ohanga.henry@gmail.com>
2021-12-08 11:05:27 +0300

    Fix graphql jwt settings for dev

--- schmsoft/settings/dev.py
+++ schmsoft/settings/dev.py
@@ -3,7 +3,10 @@ from schmsoft.settings import *
 DEBUG = True
 
 GRAPHQL_JWT = {
-    "JWT_EXPIRATION_DELTA": timedelta(days=7),
+    "JWT_VERIFY_EXPIRATION": True,
+    "JWT_LONG_RUNNING_REFRESH_TOKEN": True,
+    "JWT_EXPIRATION_DELTA": timedelta(days=3),
+    "JWT_REFRESH_EXPIRATION_DELTA": timedelta(days=7),
 }
 
 # When generating reversed URLs, use HTTP
